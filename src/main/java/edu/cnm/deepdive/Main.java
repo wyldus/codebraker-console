4972663e069e86bcf7f130b17cc97eeab6aa6adf
dagoberto <berto35572@gmail.com>
2021-06-21 10:09:02 -0600

    Started implementation of user guesses

--- src/main/java/edu/cnm/deepdive/Main.java
+++ src/main/java/edu/cnm/deepdive/Main.java
@@ -1,31 +1,58 @@
 package edu.cnm.deepdive;
 
+import edu.cnm.deepdive.model.Game;
 import edu.cnm.deepdive.service.GameRepository;
 import java.io.IOException;
 import java.util.ResourceBundle;
 import java.util.Scanner;
 
+/**
+ * Implements a simple client for a Codebreaker (similar to MasterMind, Bulls &amp; Cows) game that
+ * runs as a web service. This client uses console interaction (standard input &amp; output) to
+ * obtain guesses from the user and display the result of each guess.
+ *
+ * @author Nicholas Bennett and DDC Java + Android Bootcamp cohort 13.
+ */
 public class Main {
 
+  private static final String POOL = "AEIOUY";
+  private static final String BUNDLE_NAME = "strings";
+  private static final int CODE_LENGTH = 7;
+  private static final String REPLAY_PROMPT_KEY = "replay_prompt";
+  private static final char NEGATIVE_RESPONSE = 'n';
+
+  /**
+   * Entry point for game. Connects to Codebreaker service to start each game, and query the user
+   * for guesses and new games, until the user declines to play again.
+   *
+   * @param args Command-line arguments (currently ignored).
+   * @throws IOException If a network or network resource (such as web service) fails in sending the
+   *                     request and receiving the response.
+   */
   public static void main(String[] args) throws IOException {
     Scanner scanner = new Scanner(System.in);
-    ResourceBundle bundle = ResourceBundle.getBundle("strings");
-    GameRepository repository= new GameRepository();
-    //TODO setup proxy class for service
+    ResourceBundle bundle = ResourceBundle.getBundle(BUNDLE_NAME);
+    GameRepository repository = new GameRepository();
     do {
-
-    // TODO Play a single game.
-      repository.newGame("0123456789", 3);
-      } while (queryReplay(scanner, bundle));
-
+      // TODO Play a single game.
+      Game game = repository.newGame(POOL, CODE_LENGTH);
+      System.out.printf("Secret code of length %d generated from pool \"%s\".%n",
+          game.getLength(), game.getPool());
+    } while (queryReplay(scanner, bundle));  // End of do-while
   }
 
+  /**
+   * Prompts the user to play again. If a resource bundle exists for the current (or set) locale, a
+   * string from this resource bundle will be used for the prompt text.
+   *
+   * @param scanner Instance of {@link Scanner} used to obtain user input.
+   * @param bundle {@link ResourceBundle} holding UI string context.
+   * @return {@code false} if the user declines to play again; {@code true} otherwise.
+   */
   private static boolean queryReplay(Scanner scanner, ResourceBundle bundle) {
-    System.out.println(bundle.getString("replay_prompt"));
+    System.out.println(bundle.getString(REPLAY_PROMPT_KEY));
     String input = scanner.nextLine().trim().toLowerCase();
-    return (input.isEmpty() || input.charAt(0) != 'n');
-
-
+    return (input.isEmpty() || input.charAt(0) != NEGATIVE_RESPONSE);
   }
 
 }
--- src/main/java/edu/cnm/deepdive/service/GameRepository.java
+++ src/main/java/edu/cnm/deepdive/service/GameRepository.java
@@ -1,28 +1,59 @@
 package edu.cnm.deepdive.service;
 
 import edu.cnm.deepdive.model.Game;
+import edu.cnm.deepdive.model.Guess;
 import java.io.IOException;
-import java.lang.reflect.Proxy;
 import retrofit2.Response;
 
+/**
+ * Performs operation on instances of the model classes {@link Game} and {@link
+ * edu.cnm.deepdive.model.Guess}, including sending stub instances to the web service, and receiving
+ * completed instances.
+ */
 public class GameRepository {
 
   private final CodebreakerServiceProxy proxy;
 
+  /**
+   * Initializes this instance by obtaining an instance of {@link CodebreakerServiceProxy} to enable
+   * making requests of the web service.
+   */
   public GameRepository() {
     proxy = CodebreakerServiceProxy.getInstance();
   }
 
+  /**
+   * Creates a stub (incomplete) instance of {@link Game}, setting the fields according to {@code
+   * pool} and {@code length}, and sends this stub to the web service.
+   *
+   * @param pool
+   * @param length
+   * @return
+   * @throws IOException
+   */
   public Game newGame(String pool, int length) throws IOException {
     Game gameStub = new Game();
     gameStub.setPool(pool);
     gameStub.setLength(length);
+    // Uses a Retrofit Call object to execute the HTTP request and obtain the response.
     Response<Game> response = proxy.startGame(gameStub).execute();
+    if (!response.isSuccessful()) {
+      throw new IllegalArgumentException();
+    } // end if
+    return response.body();
+  }
+
+  public Guess newGuess(Game game, String text) throws IOException {
+    Guess guess = new Guess();
+    guess.setText(text);
+    Response<Guess> response = proxy.submitGuess(game.getId(), guess).execute();
     if (!response.isSuccessful()) {
       throw new IllegalArgumentException();
     }
     return response.body();
   }
 
-}
+  // TODO Define methods for obtaining a single existing game, the list of guesses in a game, and
+  //  submitting a new guess.
 
+}
